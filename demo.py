22df200030d04b4642679ae23076eab1a06e5652
Harshita <harshita23sharma@gmail.com>
2019-11-22 15:53:52 +0530

    person in counter detected

--- demo.py
+++ demo.py
@@ -20,19 +20,14 @@ from tools import generate_detections as gdet
 from deep_sort.detection import Detection as ddet
 warnings.filterwarnings('ignore')
 
-
 def center_point_inside_polygon(bounding_box, polygon_coord):
-    center = (int(bounding_box[0] + (bounding_box[2]) / 2), int(bounding_box[1] + (bounding_box[3]) / 2))
+    center = (int((bounding_box[0] + bounding_box[2])/2), int(bounding_box[3]))
     polygon_coord = np.array(polygon_coord, np.int32)
     polygon_coord = polygon_coord.reshape((-1, 1, 2))
     result = cv2.pointPolygonTest(polygon_coord, center, False)
-    # In the pointPolygonTest function, third argument is measureDist. If it is True, it finds the signed distance.
-    # If False, it finds whether the point is inside or outside or on the contour
-    # (it returns +1, -1, 0 respectively)
-    if result == 1:
-        return result
-    else:
-        return 0
+    if result == -1:
+        return "outside"
+    return "inside"
 
 def main(yolo):
 
@@ -50,23 +45,33 @@ def main(yolo):
 
     writeVideo_flag = True 
     
-    # video_capture = cv2.VideoCapture(0)
-    video_capture = cv2.VideoCapture('/home/harshita/Downloads/IP\ Camera8_Blr_20191121163519_20191121163647_796109.mp4')
+    video_capture = cv2.VideoCapture("/content/drive/My Drive/20191121163200-20191121163700/IP\ Camera8_Blr_20191121163519_20191121163647_796109.mp4")
 
     if writeVideo_flag:
     # Define the codec and create VideoWriter object
         w = int(video_capture.get(3))
         h = int(video_capture.get(4))
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        out = cv2.VideoWriter('op_test.avi', fourcc, 15, (w, h))
+        out = cv2.VideoWriter('output_demo1.avi', fourcc, 15, (w, h))
         list_file = open('detection.txt', 'w')
         frame_index = -1 
         
     fps = 0.0
+
+    track_dict = {}
+
     while True:
         ret, frame = video_capture.read()  # frame shape 640*480*3
         if ret != True:
             break
+
+        pts = np.array([[855,201],[1015.5,190.5],[1018.5,318],[766.5,316.5]], np.int32)
+        pts = pts.reshape((-1,1,2))
+        pts2 = np.array([[766.5,316.5],[1018.5,318.0],[952.5,712.5],[510.0,711]], np.int32)
+        pts2 = pts2.reshape((-1,1,2))
+        cv2.polylines(frame, [pts], True, (0,255,255), thickness=2)
+        cv2.polylines(frame, [pts2], True, (255,0,255), thickness=2)
+
         t1 = time.time()
 
        # image = Image.fromarray(frame)
@@ -77,14 +82,6 @@ def main(yolo):
         
         # score to 1.0 here).
         detections = [Detection(bbox, 1.0, feature) for bbox, feature in zip(boxs, features)]
-        print(type(frame) , " is the type of image")
-        #Adding region info for door and counter 
-        pts = np.array([[841.5,141.5],[1009.5,166.5],[1018.5,318],[742.5,288.0]], np.int32)
-        pts = pts.reshape((-1,1,2))
-        pts2 = np.array([[742.5,288.0],[1053.0,318.0],[1065.0,702.0],[544.5,679.5]], np.int32)
-        pts2 = pts2.reshape((-1,1,2))
-        cv2.polylines(frame,[pts],True,(0,255,255))
-        cv2.polylines(frame,[pts2],True,(255,0,255))
         
         # Run non-maxima suppression.
         boxes = np.array([d.tlwh for d in detections])
@@ -100,25 +97,37 @@ def main(yolo):
             if not track.is_confirmed() or track.time_since_update > 1:
                 continue 
             bbox = track.to_tlbr()
-            #Person detection
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
 
-            if center_point_inside_polygon(bbox,pts):
-                #Check if the person is at counter
-                #Trackid number
-                cv2.putText(frame, str(track.track_id) + str("_at_counter"),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
-            elif center_point_inside_polygon(bbox,pts2):
-                #Trackid number
-                #Check if the person is at door
-                cv2.putText(frame, str(track.track_id) + str("_at_door"),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
-            else:
-                #Trackid number
-                cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2) 
+            point_test = center_point_inside_polygon(bbox, pts)
+
+            if point_test == 'inside':
                 
+                if track.track_id not in track_dict.keys():
+                    track_dict[track.track_id] = 1
+                else:
+                    track_dict[track.track_id] += 1
+
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
+                cv2.putText(frame, str(track.track_id)+"->Frame Count:"+str(track_dict[track.track_id]),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
+
+            point_test = center_point_inside_polygon(bbox, pts2)
+
+            if point_test == 'inside':
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
+                cv2.putText(frame, str(track.track_id), (int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
 
         for det in detections:
             bbox = det.to_tlbr()
-            cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
+
+            point_test = center_point_inside_polygon(bbox, pts)
+
+            if point_test == 'inside':
+                cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
+
+            point_test = center_point_inside_polygon(bbox, pts2)
+
+            if point_test == 'inside':
+                cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
             
         cv2.imshow('', frame)
         
